a80b5a20675719bfa87e69537934bd4c2c12549e
haiduk <hwlee91@gmail.com>
2021-03-19 03:41:22 -0400

    small things before deployment

--- updated_stocks/settings/shared_settings.py
+++ updated_stocks/settings/shared_settings.py
@@ -2,14 +2,12 @@ import os
 import sys
 import dj_database_url
 from pathlib import Path
-#from stock_portfolio.settings.private_settings import SECRET_KEY
 from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
@@ -58,26 +56,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'updated_stocks.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
-#if DEVELOPMENT_MODE is True:
-#    DATABASES = {
-#        "default": {
-#            "ENGINE": "django.db.backends.sqlite3",
-#            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#        }
-#    }
-#elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-#    if os.getenv("DATABASE_URL", None) is None:
-#        raise Exception("DATABASE_URL environment variable not defined")
-#    DATABASES = {
-#        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-#    }
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
